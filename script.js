e651110cc335dd31b577125f8b354c2e3791668c
steven huynh <steven.h@live.com>
2023-11-02 19:45:06 -0700

    Add deletion and convert datetime-local to ISO8601

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ partyForm.addEventListener("submit", e => {
 	e.preventDefault();
 	const name = partyForm.name.value;
 	const description = partyForm.description.value;
-	const date = partyForm.date.value;
+	const date = new Date(partyForm.date.value).toISOString();
 	const location = partyForm.location.value;
 	addParty(name, description, date, location);
-})
+});
 
 getParty();
 
@@ -25,7 +25,7 @@ async function getParty() {
 }
 
 async function addParty(name, description, date, location) {
-	let post = await fetch(url, {
+	await fetch(url, {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({
@@ -38,14 +38,23 @@ async function addParty(name, description, date, location) {
 	getParty();
 }
 
+async function deleteParty(id) {
+	await fetch(`${url}/${id}`, {
+		method: "DELETE"
+	});
+	getParty();
+}
+
 function render() {
 	const partyRender = parties.map(party => {
 		const partyContainer = document.createElement("div");
 		partyContainer.innerHTML = `
-			<h3>${party.name}</h3>
-			<p>${party.description}</p>
-			<p>${party.date}</p>
-			<p>${party.location}</p>`;
+				<h3>${party.name}</h3>
+				<p>${party.description}</p>
+				<p>${party.date}</p>
+				<p>${party.location}</p>
+				<button onclick="deleteParty(${party.id})">Delete</button>
+			`;
 		return partyContainer;
 	});
 	partyList.replaceChildren(...partyRender);
